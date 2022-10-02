914140b67df9db7aac652577035e86d2c2f6c131
Ian Stidsen <ianlundstidsen@gmail.com>
2022-10-02 18:46:18 -0200

    functioning 2.0

--- script.js
+++ script.js
@@ -6,8 +6,23 @@ setTime()
 setInterval(setTime, 1000)
 
 function setTime() {  
-  const currentTime = new Date()
-  hours.innerText = String(currentTime.getHours() + ':')
-  minutes.innerText = String(currentTime.getMinutes() + ':')
-  seconds.innerText = String(currentTime.getSeconds())
+  const currentTime = new Date();
+  const hour = String(currentTime.getHours() + ':');
+  const minute = String(currentTime.getMinutes() + ':');
+  const second = String(currentTime.getSeconds());
+  if (hour.length === 1) {
+    hours.innerText = '0' + hour
+  } else {
+    hours.innerText = hour
+  }
+  if (minute.length === 1) {
+    hours.innerText = '0' + hour
+  } else {
+    minutes.innerText = minute
+  }
+  if (second.length === 1) {
+    seconds.innerText = '0' + second
+  } else {
+    seconds.innerText = second
+  }
 }

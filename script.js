3873d59459590279b272d41ad1421cfd375f9002
Ian Stidsen <ianlundstidsen@gmail.com>
2022-10-02 15:08:09 -0200

    functional

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
+const hours = document.getElementById('hours')
+const minutes = document.getElementById('minutes')
+const seconds = document.getElementById('seconds')
 
-  
+setTime()
+setInterval(setTime, 1000)
+
+function setTime() {  
+  const currentTime = new Date()
+  hours.innerText = String(currentTime.getHours() + ':')
+  minutes.innerText = String(currentTime.getMinutes() + ':')
+  seconds.innerText = String(currentTime.getSeconds())
+}

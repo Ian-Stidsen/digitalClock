4774e7d21d2ae0071770eb64f39baa0a357425a4
Ian Stidsen <ianlundstidsen@gmail.com>
2022-10-04 13:55:04 -0200

    added timezones

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
 const hours = document.getElementById('hours')
 const minutes = document.getElementById('minutes')
 const seconds = document.getElementById('seconds')
+const timezone = document.getElementById('timezone-menu')
 
+setLocalTimezone()
 setTime()
-setInterval(setTime, 1000)
+setInterval(setTime, 500)
 
 function setTime() {  
+  const timezoneValue = parseInt(timezone.options[timezone.selectedIndex].value);
   const currentTime = new Date();
-  const hour = String(currentTime.getHours() + ':');
-  const minute = String(currentTime.getMinutes() + ':');
-  const second = String(currentTime.getSeconds());
-  if (hour.length === 1) {
-    hours.innerText = '0' + hour
+  const setTimezone = currentTime.setUTCHours(currentTime.getUTCHours() + timezoneValue);
+  const hour = currentTime.getUTCHours();
+  const minute = currentTime.getMinutes();
+  const second = currentTime.getSeconds();
+  if (String(hour).length === 1) {
+    hours.innerText = '0' + hour + ':';
   } else {
-    hours.innerText = hour
+    hours.innerText = hour + ':';
   }
-  if (minute.length === 1) {
-    hours.innerText = '0' + hour
+  if (String(minute).length === 1) {
+    minutes.innerText = '0' + minute + ':';
   } else {
-    minutes.innerText = minute
+    minutes.innerText = minute + ':';
   }
-  if (second.length === 1) {
+  if (String(second).length === 1) {
     seconds.innerText = '0' + second
   } else {
     seconds.innerText = second
   }
+}
+
+function setLocalTimezone() {
+  const time = new Date();
+  const localTime = time.getHours() - time.getUTCHours();
+  timezone[0].value = localTime;
+  timezone[0].innerText = 'Local timezone ' + localTime;
 }
